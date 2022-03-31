3e8c14d51a47b49af8e2071507d1c17677ec4c56
filippov_filipp <imawakebrba@gmail.com>
2022-03-31 18:29:49 +0700

    comm

--- Lab7/Form1.cs
+++ Lab7/Form1.cs
@@ -75,49 +75,35 @@ namespace Lab7
         public int Staff => _staff;
         public int Population => _population;
         public double Budget => _budget;
-
-        /// <summary>
-        /// Полицейский участок
-        /// </summary>
-        /// <param name="budget">Бюджет</param>
+        
         public PoliceDepartment(double budget, int population)
-        {
-            //Бюджет вычисляется в 1т.р
+        {            
             _budget = budget;
             _population = population;
-            //На каждого сотрудника в среднем выделяется 100т.р.
-            _staff = Convert.ToInt32(_budget / 100);
+            _staff = Convert.ToInt32(_budget / 200);
             _detectives = Convert.ToInt32(_staff * 0.4);
             _patrols = Convert.ToInt32(_staff * 0.4);
-            _preventions = Convert.ToInt32(_staff * 0.2);
-            //Уровень безопасности/жизни равен единице, когда количество сотрудников полиции покрывает всю численность населения. В среднем один сотрудник эквивалентен 50 гражданам.
+            _preventions = Convert.ToInt32(_staff * 0.2);            
             _lifeQuality = _staff * 75.0 / _population;
+            _lifeQuality = _lifeQuality >= 1 ? 1 : _lifeQuality;
         }
 
         private void CityDataUpdate()
-        {
-            //В зависимости от прироста населения прирост бюджета варьируется от -10% до 10%
-            var dif = 1 + 0.2 * (_random.NextDouble() - 0.5);
+        {            
+            var dif = 1 + 0.1 * (_random.NextDouble() - 0.5);
             _population = Convert.ToInt32(dif * _population);
-            _budget = Convert.ToInt32(dif * _budget);
-            _staff = Convert.ToInt32(dif * _staff);
-            _detectives = Convert.ToInt32(dif * _detectives);
-            _patrols = Convert.ToInt32(dif * _patrols);
-            _preventions = Convert.ToInt32(dif * _preventions);
-            //
+            _budget = Convert.ToInt32(dif * _budget * _lifeQuality);
+
+            _staff = Convert.ToInt32(_budget / 200);
+            _detectives = Convert.ToInt32(_staff * 0.4);
+            _patrols = Convert.ToInt32(_staff * 0.4);
+            _preventions = Convert.ToInt32(_staff * 0.2);
             var politQuality = 2.5 * _preventions / _population;
-            //Качество жизни/безопасноти варьируется в пределах от -2.5% до 2.5% и увеличивается в зависимости от качества профилактики
+
             _lifeQuality += (0.05 * (_random.NextDouble() - 0.5)) + politQuality;
             _lifeQuality = _lifeQuality >= 1 ? 1 : _lifeQuality;
-            //Количество инцидентов зависит от уровня жизни/безопасноти и населения. При максимальном показателе уровня жизни на каждые 500 человек происходит 1 инцидент
+
             _incidents = Convert.ToInt32(_population / _lifeQuality / 500);
-
-            //Популяция растет на рандомное число процентов(от -10% до 10%)
-            //На тот же процент растет бюджет
-            //ПОлитруки влияют на качество жизни
-            //Качество жизни растет на рандомное число процентов(от -10% до 10%)
-            //Вычисляется количество инцидентов
-
             _mounthOfWork++;
 
         }
@@ -131,50 +117,47 @@ namespace Lab7
             _arrests = 0;
             var prevIncidents = _incidents;
 
-            //патрули
             var streetIncidents = Convert.ToInt32(0.25f * _incidents);
             _incidents -= (streetIncidents - _patrols) >= 0 ? _patrols : streetIncidents;
 
-            //следователи
-            
             var criminalIncidents = Convert.ToInt32(0.75f * prevIncidents);
             var incPerDet = criminalIncidents / _detectives;
-            for(int i = 0; i < _detectives;i++)
+            for (int i = 0; i < _detectives; i++)
             {
-                for(int j = 0; j < incPerDet; j++)
+                for (int j = 0; j < incPerDet; j++)
                 {
                     var chance = _random.NextDouble();
-                    if(chance <= 0.5f)
+                    if (chance <= 0.5f)
                     {
-                        _incidents--;                        
+                        _incidents--;
                     }
                 }
             }
-            for(int i = 0;i < (prevIncidents - _incidents); i++)
+            for (int i = 0; i < (prevIncidents - _incidents); i++)
             {
                 CloseDeal();
             }
-            //генерация арестов и штрафов, в зависомости от сотрудников
+            _incidents = prevIncidents;
         }
 
         private void CloseDeal()
         {
             int penalty = 0;
             var penaltyChance = _random.NextDouble();
-            if(penaltyChance <= 0.5f)
+            if (penaltyChance <= 0.5f)
             {
-                penalty = _random.Next(500, 1000000);
+                penalty = _random.Next(500, 1000);
                 _amountOfFines += penalty;
                 _budget += penalty;
             }
-            if(penalty == 0)
+            if (penalty == 0)
             {
                 _arrests++;
             }
-            else 
+            else
             {
                 var arrestChance = _random.NextDouble();
-                if(arrestChance <= 0.5f)
+                if (arrestChance <= 0.5f)
                 {
                     _arrests++;
                 }
